cdca5c825030817371d7d80168d600e9c18b7136
Shelby Rothman <shelby.rothman@aderant.com>
2023-12-10 17:11:31 -0800

    solidifying typings

--- src/components/ClearControlTextInput.js
+++ src/components/ClearControlTextInput.js
@@ -17,24 +17,29 @@ import {
 export function ClearControlTextInput(props) {
     const [ inFocus, setInFocus ] = React.useState(false);
     const textInputRef = useRef(null);
-    // const textColor = props.style === 'dark' ? 'white' : 'black';
-
     const iconElement = props.icon ? props.icon : <Octicons name="x-circle-fill" size={16} color="#ccc8c8" />;
 
 
     const conditionalMode = () => {
         switch (props.mode) {
-            case 'always':
-                //  if its always then it should be visible whether or not its in focus
-                return true;
-            case 'while-editing':
-                // if its while editing then it should be visible only when its in focus
-                return inFocus;
-            case 'unless-editing':
-                // if its unless-editing then it should be visible only when its not in focus
-                return !inFocus;
+            //  if its always then it should be visible whether or not its in focus
+            case 'always': return true;
+            // if its while editing then it should be visible only when its in focus
+            case 'while-editing': return inFocus;
+            // if its unless-editing then it should be visible only when its not in focus
+            case 'unless-editing': return !inFocus;
             default: return inFocus;
         }
+    };
+
+    // TODO: test these out
+    const handleFocus = () => {
+        setInFocus(true);
+        props.onFocus && props.onFocus();
+    };
+    const handleBlur = () => {
+        setInFocus(false);
+        props.onBlur && props.onBlur();
     };
 
     return (
@@ -45,53 +50,18 @@ export function ClearControlTextInput(props) {
             ]}
         >
             <TextInput
-                onChangeText={props.onChangeText}
-                placeholder={props.placeholder}
-                value={props.value}
+                // onChangeText={props.onChangeText}
+                // placeholder={props.placeholder}
+                // value={props.value}
                 style={[
                     styles.input,
                     { color: props.textColor }
                 ]}
-                multiline={props.multiline}
                 ref={textInputRef}
-                placeholderTextColor={props.placeholderTextColor}
-                onFocus={() => setInFocus(true)}
-                onBlur={() => setInFocus(false)}
-                secureTextEntry={props.secureTextEntry}
-                keyboardType={props.keyboardType}
-                returnKeyType={props.returnKeyType}
-                // its like i want all of them ezxcept the ones specific to ios and android...
-                // TODO: these can just be a spresad but putthese al;l in type file./..
-                allowFontScaling={props.allowFontScaling}
-                autoCapitalize={props.autoCapitalize}
-                autoComplete={props.autoComplete}
-                autoCorrect={props.autoCorrect}
-                autoFocus={props.autoFocus}
-                blurOnSubmit={props.blurOnSubmit}
-                caretHidden={props.caretHidden}
-                contextMenuHidden={props.contextMenuHidden}
-                defaultValue={props.defaultValue}
-                editable={props.editable}
-                enterKeyHint={props.enterKeyHint}
-                inputMode={props.inputMode}
-                maxFontSizeMultiplier={props.maxFontSizeMultiplier}
-                maxLength={props.maxLength}
-                onChange={props.onChange}
-                onContentSizeChange={props.onContentSizeChange}
-                onEndEditing={props.onEndEditing}
-                onPressIn={props.onPressIn}
-                onPressOut={props.onPressOut}
-                onKeyPress={props.onKeyPress}
-                onLayout={props.onLayout}
-                onScroll={props.onScroll}
-                onSelectionChange={props.onSelectionChange}
-                onSubmitEditing={props.onSubmitEditing}
-                readOnly={props.readOnly}
-                selection={props.selection}
-                selectionColor={props.selectionColor}
-                selectTextOnFocus={props.selectTextOnFocus}
-                showSoftInputOnFocus={props.showSoftInputOnFocus}
-                textAlign={props.textAlign}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                // onBlur={() => setInFocus(false)}
+                {...props.textInputProps}
             />
 
             {conditionalMode() && <Pressable
