0b6c492540a9e0982850d6e92c2f8b7e3ba1f99c
shelby.rothman <shelby.rothman@aderant.com>
2023-12-17 15:04:42 -0800

    task(unit-test): Unit Test suite for custom color props

--- test-suite/__Tests__/ClearControlTextInput/Colors.test.js
+++ test-suite/__Tests__/ClearControlTextInput/Colors.test.js
@@ -11,105 +11,103 @@ import { ClearControlTextInput } from 'expo-clear-input-test';
 const defautltProps = {
     textInputProps: {
         onChangeText: () => { },
-        value: '',
         placeholder: 'some search word',
+        placeholderTextColor: undefined,
     },
     backgroundColor: undefined,
     textColor: undefined,
-    placeholderTextColor: undefined,
     iconColor: undefined,
+    showButtonMode: 'always'
 };
 
-// states of the colors: the string or undefined or an non-hexadecimal string which will...
-// color, undefined/default, non-hex(render as default)
-
-// conditioons:
-// valid color for each then
-////// textcolor
-    // textInput style.color = color
-///// backgroundColor
-    // view.props.style.backgroundColor = color
-//// placeholderTextColor
-    // textInput.props.style.placeholderTextColor = color
-///// iconColor
-    // icon.props.color = color
-
-// both not provided AND for the non-hex
-////// textcolor
-    // textInput style.color = ""
-///// backgroundColor
-    // view.props.style.backgroundColor = color
-//// placeholderTextColor
-    // textInput.props.style.placeholderTextColor = color
-///// iconColor
-    // icon.props.color = color
+/** default colors per definition */
+const DEFAULT_TEXT = '#fff';
+const DEFAULT_BACKGROUND = '#252326';
+const DEFAULT_PLACEHOLDER = '#ccc8c8';
+const DEFAULT_ICON = '#ccc8c8';
+/** custom colors */
+const PINK = '#ff00ff';
+const GREEN = '#00ff00';
+const BLUE = '#0000ff';
 
 
-describe('verticalPlacement prop', () => {
+
+describe('Colors: backgroundColor, textColor, iconColor, placeholderTextColor', () => {
     /** 
      * componentNode in this scope is the <View> wrapper of the module 
      * componentNode.toJSON().children[0] = the text input
      * componentNode.toJSON().children[1] = the pressable
     */
     let componentNode;
-
-    /** undefined (should behave identical to while-editing) */
-    describe('undefined/default', () => {
+    /** undefined/default */
+    describe('undefined/default values', () => {
         beforeEach(() => {
             act(() => componentNode = renderer.create(<ClearControlTextInput {...defautltProps} />));
         });
-        it('the pressable should not appear when the text input is not focused', () => {
-            act(() => componentNode.toJSON().children[ 0 ].props.onBlur());
-            expect(componentNode.toJSON().children.length).toBe(1);
+        // text input color
+        it('color of the TextInputs text should be the default', () => {
+            expect(componentNode.toJSON().children[ 0 ].props.style[ 1 ].color).toBe(DEFAULT_TEXT);
         })
-        it('the pressable should appear when the text input is focused', () => {
-            act(() => componentNode.toJSON().children[ 0 ].props.onFocus());
-            expect(componentNode.toJSON().children.length).toBe(2);
+        // background color for view
+        it('color of the Views background should be the default', () => {
+            expect(componentNode.toJSON().props.style[ 1 ].backgroundColor).toBe(DEFAULT_BACKGROUND);
+        })
+        // pickup: figure out a way to test the actual prop of "placeHolderTextColor"
+        //icon  
+        it('color of the Icon should be the default', () => {
+            console.log(componentNode.toJSON().children[ 1 ].children[ 0 ].props.style[ 0 ].color)
+            expect(componentNode.toJSON().children[ 1 ].children[ 0 ].props.style[ 0 ].color).toBe(DEFAULT_ICON);
         })
     })
-    /** while-editing */
-    // describe('while-editing', () => {
-    //     beforeEach(() => {
-    //         act(() => componentNode = renderer.create(<ClearControlTextInput {...defautltProps} />));
-    //     });
-    //     it('the pressable should not appear when the text input is not focused', () => {
-    //         act(() => componentNode.toJSON().children[ 0 ].props.onBlur());
-    //         expect(componentNode.toJSON().children.length).toBe(1);
-    //     })
-    //     it('the pressable should appear when the text input is focused', () => {
-    //         act(() => componentNode.toJSON().children[ 0 ].props.onFocus());
-    //         expect(componentNode.toJSON().children.length).toBe(2);
-    //     })
-    // })
-    // /** unless-editing */
-    // describe('unless-editing', () => {
-    //     beforeAll(() => defautltProps.showButtonMode = 'unless-editing');
-    //     beforeEach(() => {
-    //         act(() => componentNode = renderer.create(<ClearControlTextInput {...defautltProps} />));
-    //     });
-    //     it('the pressable should appear when the text input is not focused', () => {
-    //         act(() => componentNode.toJSON().children[ 0 ].props.onBlur());
-    //         expect(componentNode.toJSON().children.length).toBe(2);
-    //     })
-    //     it('the pressable should not appear when the text input is focused', () => {
-    //         act(() => componentNode.toJSON().children[ 0 ].props.onFocus());
-    //         expect(componentNode.toJSON().children.length).toBe(1);
-    //     })
-    // })
-    // /** always */
-    // describe('always', () => {
-    //     beforeAll(() => defautltProps.showButtonMode = 'always');
-    //     beforeEach(() => {
-    //         act(() => componentNode = renderer.create(<ClearControlTextInput {...defautltProps} />));
-    //     });
-    //     it('the pressable should appear even when the text input is not focused', () => {
-    //         act(() => componentNode.toJSON().children[ 0 ].props.onBlur());
-    //         expect(componentNode.toJSON().children.length).toBe(2);
-    //     })
-    //     it('the pressable should appear when the text input is focused', () => {
-    //         act(() => componentNode.toJSON().children[ 0 ].props.onFocus());
-    //         expect(componentNode.toJSON().children.length).toBe(2);
-    //     })
-    // })
+    /** undefined/default */
+    describe('invalid color value for each', () => {
+        beforeEach(() => {
+            defautltProps.textColor = 'not ###a color';
+            defautltProps.backgroundColor = 'not a49083240 color';
+            // defautltProps.textInputProps.placeholderTextColor = '#n##ot a color';
+            defautltProps.iconColor = '#&&*(&(*$#$';
+            act(() => componentNode = renderer.create(<ClearControlTextInput {...defautltProps} />));
+        });
+        // text input color
+        it('color of the TextInputs text should be the default', () => {
+            expect(componentNode.toJSON().children[ 0 ].props.style[ 1 ].color).toBe(DEFAULT_TEXT);
+        })
+        // background color for view
+        it('color of the Views background should be the default', () => {
+            expect(componentNode.toJSON().props.style[ 1 ].backgroundColor).toBe(DEFAULT_BACKGROUND);
+        })
+        //icon  
+        it('color of the Icon should be the default', () => {
+            expect(componentNode.toJSON().children[ 1 ].children[ 0 ].props.style[ 0 ].color).toBe(DEFAULT_ICON);
+        })
+        // pickup: figure out a way to test the actual prop of "placeHolderTextColor"
+    })
+    /** valid color for each */
+    describe('custom color value for each', () => {
+        beforeEach(() => {
+            defautltProps.textColor = PINK;
+            defautltProps.backgroundColor = GREEN;
+            defautltProps.iconColor = BLUE;
+            act(() => componentNode = renderer.create(<ClearControlTextInput {...defautltProps} />));
+        });
+        // text input color
+        it('color of the TextInputs text should be the default', () => {
+            expect(componentNode.toJSON().children[ 0 ].props.style[ 1 ].color).toBe(PINK);
+        })
+        // background color for view
+        it('color of the Views background should be the default', () => {
+            expect(componentNode.toJSON().props.style[ 1 ].backgroundColor).toBe(GREEN);
+        })
+        //icon  
+        it('color of the Icon should be the default', () => {
+            expect(componentNode.toJSON().children[ 1 ].children[ 0 ].props.style[ 0 ].color).toBe(BLUE);
+        })
+        // pickup: figure out a way to test the actual prop of "placeHolderTextColor"
+    })
+
+
+
+
+    /** non-hex values provided */
 
 })
